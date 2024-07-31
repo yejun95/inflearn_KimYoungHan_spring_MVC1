41068845d1b2a61ad88d0a91549708ba8171d09e
xodlf100 <xodlf100@gmail.com>
2024-08-01 02:06:43 +0900

    servletì - api 형식으로 text 출력 * postman을 통한 테스트 진행

---
+++ src/main/java/hello/servlet/basic/request/RequestBodyStringServlet.java
@@ -0,0 +1,24 @@
+package hello.servlet.basic.request;
+
+import jakarta.servlet.ServletInputStream;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.util.StreamUtils;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@WebServlet(name = "requestBodyStringServlet", urlPatterns = "/request-body-string")
+public class RequestBodyStringServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        ServletInputStream inputStream = req.getInputStream();
+        String messagebody = StreamUtils.copyToString(inputStream , StandardCharsets.UTF_8);
+
+        System.out.println("messagebody = " + messagebody);
+        res.getWriter().write("OK");
+    }
+}
