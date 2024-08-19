1426f179a301e3263930d4de0044771223398f0a
xodlf100 <xodlf100@gmail.com>
2024-08-20 01:50:23 +0900

    add @RequestMapping MVC pattern

---
+++ src/main/java/hello/servlet/web/springmvc/SpringMemberListControllerV1.java
@@ -0,0 +1,31 @@
+package hello.servlet.web.springmvc;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+public class SpringMemberListControllerV1 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @RequestMapping("/springmvc/v1/members")
+    public ModelAndView process() {
+        List<Member> members = memberRepository.findByAll();
+
+        ModelAndView mv = new ModelAndView("members");
+        mv.addObject("members", members);
+        return mv;
+    }
+}
